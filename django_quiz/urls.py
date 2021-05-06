9bcd78df40053fa6b35548bae12e6f5b11a1e63c
Dragan Manojlovic <dragojlo@net.hr>
2021-05-07 00:07:05 +0200

    Initial

--- django_quiz/urls.py
+++ django_quiz/urls.py
@@ -22,7 +22,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
-    path('quiz/', include('quiz.urls')),
+    path('', include('quiz.urls')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
