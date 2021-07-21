f57539b984c8ee51a35c6002b7094e90892d3140
dmanojlo <dragojlo@net.hr>
2021-07-21 12:10:39 +0200

    ini

--- django_quiz/settings.py
+++ django_quiz/settings.py
@@ -195,7 +195,7 @@ CHANNEL_LAYERS = {
     "default": {
         "BACKEND": "channels_redis.core.RedisChannelLayer",
         "CONFIG": {
-            "hosts": [os.environ.get('REDIS_URL', 6379)],
+            "hosts": [('quisko.herokuapp.com', 6379)],
         },
     },
 }
